5dde87c4e26a3eeae12cb9e288278605986ebd6f
tannu945 <tanishaagrawal2015@gmail.com>
2023-02-20 17:55:15 +0530

    error: module not found.

--- action_server_btree/btree/GetTip.py
+++ action_server_btree/btree/GetTip.py
@@ -1,6 +1,10 @@
+"""Make sequence and selector of nodes from tree as node class is gettipnode.   """
+
+from zenoh import QueryTarget #type: ignore
+from pydantic import BaseModel #type: ignore
+from behaviourTree.node import Node, NodeState #type: ignore
 import logging
-import zenoh
-from zenoh import QueryTarget
+import zenoh #type: ignore
 
 logging.getLogger().setLevel(logging.DEBUG)
 
@@ -11,6 +15,59 @@ def get_status(key_expression: str):
         logging.debug("Received reply: {}".format(reply))
         return reply.ok.payload.decode("utf-8")
 
+class GetTipNode(BaseModel):
+    prepare_tip_for_pickup: str = "prepare_tip_for_pickup"
+    pick_up_using_orchestraror: str = "pick_up_using_orchestraror"
+    tip_available: str= "tip_available"
+    pick_up_success: str= "pick_up_success"
+    tip_available_in_tray: str= "tip_available_in_tray"
+    move_tip_slider_to_pos: str= "move_tip_slider_to_pos"
+    pick_up: str= "pick_up"
+    caught_tip_firm_and_orient: str = "caught_tip_firm_and_orient"
+    discard_current_tray: str = "discard_current_tray"
+    load_new_tray: str = "load_new_tray"
+    load_success: str = "load_success"
+    already_in_pos: str = "already_in_pos"
+    move_tip_slider: str = "move_tip_slider"
+    slider_reached: str = "slider_reached"
+    discard_tip: str = "discard_tip"
+    tray_avaialble: str = "tray_avaialble"
+    slider_move_to_load: str = "slider_move_to_load"
+    load_next_tray: str = "load_next_tray"
+    goto_discard_position: str = "goto_discard_position"
+    prepare_to_discard: str = "prepare_to_discard"
+    eject_tip: str = "eject_tip"
+    discard_tip_success: str = "discard_tip_success"
+    retry_count_below_threshold: str = "retry_count_below_threshold"
+
+class GetTip(Node):
+    def __init__(self) -> None:
+        pass
+    
+    def decide_hardware_module(self, node):
+        hardware_module = False
+        tiprm = ["prepare_tip_for_pickup", "pick_up_using_orchestraror", "tip_available", "pick_up_success", "tip_available_in_tray", "move_tip_slider_to_pos"]
+        tipchecker = ["pick_up", "caught_tip_firm_and_orient", "discard_current_tray", "load_new_tray", "load_success", "already_in_pos", "move_tip_slider", "slider_reached", "discard_tip", "tray_avaialble", "slider_move_to_load", "load_next_tray", "goto_discard_position", "prepare_to_discard", "eject_tip", "discard_tip_success", "retry_count_below_threshold"]
+        orchestrator = ["pick_up", "caught_tip_firm_and_orient", "discard_current_tray", "load_new_tray", "load_success", "already_in_pos", "move_tip_slider", "slider_reached", "discard_tip", "tray_avaialble", "slider_move_to_load", "load_next_tray", "goto_discard_position", "prepare_to_discard", "eject_tip", "discard_tip_success", "retry_count_below_threshold"]
+        pipette = ["pick_up", "caught_tip_firm_and_orient", "discard_current_tray", "load_new_tray", "load_success", "already_in_pos", "move_tip_slider", "slider_reached", "discard_tip", "tray_avaialble", "slider_move_to_load", "load_next_tray", "goto_discard_position", "prepare_to_discard", "eject_tip", "discard_tip_success", "retry_count_below_threshold"]
+        if node in tiprm:
+            hardware_module = "TipRM"
+        elif node in tipchecker:
+            hardware_module = "TipChecker"
+        elif node in orchestrator:
+            hardware_module = "Orchestrator"
+        elif node in pipette:
+            hardware_module = "Pipette"
+        return hardware_module
+
+    def Evaluate(self, timestamp, node) -> str:
+        value = self.decide_hardware_module(node)
+        if value:
+            response = get_status(f"{value}/trigger?timestamp={timestamp}&event={node}")
+            if response['response_type'] == "rejected":
+                return "Issue with trigger."
+        return "Trigger is triggered."
+
 class Get_Tip():
     def prepare_tip_for_pickup(self, timestamp) -> str:
         return "prepare_tip_for_pickup"
@@ -20,7 +77,7 @@ class Get_Tip():
         expr = f"TipRM/trigger?timestamp={timestamp}&event=tip_available"
         value = get_status(expr)
         return value
-    def pick_up_success(self, timestamp) -> str:
+    def pick_up_success(self, timestamp=123456789) -> str:
         expr = f"TipRM/trigger?timestamp={timestamp}&event=pick_up_success"
         value = get_status(expr)
         return value
@@ -110,4 +167,3 @@ class Get_Tip():
             return value
     def retry_count_below_threshold(self, timestamp) -> str:
         return "retry_count_below_threshold"
- 
