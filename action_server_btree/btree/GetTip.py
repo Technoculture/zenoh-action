7b776775fc092c1a3956ae3830cd937630d55f60
tannu945 <tanishaagrawal2015@gmail.com>
2023-02-21 18:39:00 +0530

    chore: not needed file, have some extra classes.

--- action_server_btree/btree/GetTip.py
+++ action_server_btree/btree/GetTip.py
@@ -1,19 +1,9 @@
-"""Make sequence and selector of nodes from tree as node class is gettipnode.   """
-
 from zenoh import QueryTarget #type: ignore
 from pydantic import BaseModel #type: ignore
-from behaviourTree.node import Node, NodeState #type: ignore
 import logging
 import zenoh #type: ignore
 
 logging.getLogger().setLevel(logging.DEBUG)
-
-def get_status(key_expression: str):
-    session = zenoh.open(zenoh.Config())
-    replies = session.get(key_expression, zenoh.Queue(), QueryTarget.ALL())
-    for reply in replies:
-        logging.debug("Received reply: {}".format(reply))
-        return reply.ok.payload.decode("utf-8")
 
 class GetTipNode(BaseModel):
     prepare_tip_for_pickup: str = "prepare_tip_for_pickup"
@@ -39,34 +29,17 @@ class GetTipNode(BaseModel):
     eject_tip: str = "eject_tip"
     discard_tip_success: str = "discard_tip_success"
     retry_count_below_threshold: str = "retry_count_below_threshold"
+    discard_success: str = "discard_success"
 
-class GetTip(Node):
-    def __init__(self) -> None:
-        pass
-    
-    def decide_hardware_module(self, node):
-        hardware_module = False
-        tiprm = ["prepare_tip_for_pickup", "pick_up_using_orchestraror", "tip_available", "pick_up_success", "tip_available_in_tray", "move_tip_slider_to_pos"]
-        tipchecker = ["pick_up", "caught_tip_firm_and_orient", "discard_current_tray", "load_new_tray", "load_success", "already_in_pos", "move_tip_slider", "slider_reached", "discard_tip", "tray_avaialble", "slider_move_to_load", "load_next_tray", "goto_discard_position", "prepare_to_discard", "eject_tip", "discard_tip_success", "retry_count_below_threshold"]
-        orchestrator = ["pick_up", "caught_tip_firm_and_orient", "discard_current_tray", "load_new_tray", "load_success", "already_in_pos", "move_tip_slider", "slider_reached", "discard_tip", "tray_avaialble", "slider_move_to_load", "load_next_tray", "goto_discard_position", "prepare_to_discard", "eject_tip", "discard_tip_success", "retry_count_below_threshold"]
-        pipette = ["pick_up", "caught_tip_firm_and_orient", "discard_current_tray", "load_new_tray", "load_success", "already_in_pos", "move_tip_slider", "slider_reached", "discard_tip", "tray_avaialble", "slider_move_to_load", "load_next_tray", "goto_discard_position", "prepare_to_discard", "eject_tip", "discard_tip_success", "retry_count_below_threshold"]
-        if node in tiprm:
-            hardware_module = "TipRM"
-        elif node in tipchecker:
-            hardware_module = "TipChecker"
-        elif node in orchestrator:
-            hardware_module = "Orchestrator"
-        elif node in pipette:
-            hardware_module = "Pipette"
-        return hardware_module
+"""    
+#Method: 2
 
-    def Evaluate(self, timestamp, node) -> str:
-        value = self.decide_hardware_module(node)
-        if value:
-            response = get_status(f"{value}/trigger?timestamp={timestamp}&event={node}")
-            if response['response_type'] == "rejected":
-                return "Issue with trigger."
-        return "Trigger is triggered."
+def get_status(key_expression: str):
+    session = zenoh.open(zenoh.Config())
+    replies = session.get(key_expression, zenoh.Queue(), QueryTarget.ALL())
+    for reply in replies:
+        logging.debug("Received reply: {}".format(reply))
+        return reply.ok.payload.decode("utf-8")
 
 class Get_Tip():
     def prepare_tip_for_pickup(self, timestamp) -> str:
@@ -167,3 +140,4 @@ class Get_Tip():
             return value
     def retry_count_below_threshold(self, timestamp) -> str:
         return "retry_count_below_threshold"
+"""
