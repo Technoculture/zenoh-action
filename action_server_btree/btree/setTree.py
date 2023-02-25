ebd4b64e9210cf1a4905fff7e11a76ebc062b72d
tannu945 <tanishaagrawal2015@gmail.com>
2023-02-25 18:51:17 +0530

    chore: updated tree

--- action_server_btree/btree/setTree.py
+++ action_server_btree/btree/setTree.py
@@ -1,13 +1,26 @@
 from tree import Tree, Selector, Sequence
 from node import Node, NodeState
 
+stack: dict = {"pickup":0, "tip_availability":0, "tip": 0}
+waiting: int = 0
+
 class pickup(Node):
     def __init__(self):
         super().__init__()
     
     def Evaluate(self) -> NodeState:
-        print("pickup")
-        state = NodeState.RUNNING
+        global waiting
+        if stack.get("pickup") == 0:
+            state = NodeState.RUNNING
+            waiting += 1
+        elif waiting < 10:
+            state = NodeState.RUNNING
+            waiting += 1
+        elif stack.get("pickup") == 1:
+            state = NodeState.SUCCESS
+        elif waiting > 10:
+            state = NodeState.FAILURE
+            waiting = 0
         return state
 
 class tip_availability(Node):
@@ -15,12 +28,19 @@ class tip_availability(Node):
         super().__init__()
     
     def Evaluate(self) -> NodeState:
-        print("tip_availability")
-        count = 1
-        if count == 0:
-            state = NodeState.SUCCESS
-        else:
+        global waiting
+        if stack.get("tip_availability") == 0:
+            stack["pickup"] = 1
             state = NodeState.RUNNING
+            waiting += 1
+        elif stack.get("tip_availability") == 1:
+            state = NodeState.SUCCESS
+        elif waiting > 10:
+            state = NodeState.FAILURE
+            waiting = 0
+        elif waiting < 10:
+            state = NodeState.RUNNING
+            waiting += 1
         return state
     
 class tip(Node):
@@ -28,26 +48,24 @@ class tip(Node):
         super().__init__()
     
     def Evaluate(self) -> NodeState:
-        print("tip")
-        count = 1
-        if count == 0:
-            state = NodeState.SUCCESS
-        else:
+        global waiting
+        if stack.get("tip") == 0:
+            stack["tip_availability"] = 1
             state = NodeState.RUNNING
+            waiting += 1
+        elif stack.get("tip") == 1:
+            state = NodeState.SUCCESS
+        elif waiting > 10:
+            state = NodeState.FAILURE
+            waiting = 0
+        elif waiting < 10:
+            state = NodeState.RUNNING
+            waiting += 1
         return state
-
+    
 class SetTree(Tree):
     def SetupTree(self):
         # Create the tree structure
         root = Selector([pickup(), 
                          Sequence([tip_availability(), tip()])])
         return root
-
-if __name__ == "__main__":
-    tree = SetTree()
-    tree.Start()
-    root = tree.SetupTree()
-    value = root.Evaluate()
-    print(value)
-    _value = root.Evaluate()
-    print(_value)
