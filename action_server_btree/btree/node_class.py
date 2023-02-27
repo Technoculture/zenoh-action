1942f8722cafc60845e699f807b4cb7d81c3a538
tannu945 <tanishaagrawal2015@gmail.com>
2023-02-27 18:19:48 +0530

    feat: added classes to evaluate root and leaf node.

---
+++ action_server_btree/btree/node_class.py
@@ -0,0 +1,32 @@
+from typing import Any
+from node import Node, NodeState
+
+"""Global Variables"""
+leaf_dict: list = []
+retry_count: int = 0
+
+class non_leaf_node(Node):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def Evaluate(self, node: Any, timestamp: str) -> NodeState:
+        if Node().getData(node) == "NodeState.RUNNING":
+            leaf_node_exists = Node.children[Node.children.index(node) + 1]
+            if leaf_node_exists in leaf_dict:
+                Node().setData(node, "NodeState.SUCCESS")
+                return NodeState.SUCCESS
+            return NodeState.RUNNING
+        elif Node().getData(node) == None:
+            Node().setData(node, "NodeState.RUNNING")
+            return NodeState.RUNNING
+        else:
+            Node().setData(node, "NodeState.FAILURE")
+            return NodeState.FAILURE
+        
+class leaf_node(Node):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def Evaluate(self, node: Any, timestamp: Any) -> NodeState:
+        return NodeState.SUCCESS
+    
