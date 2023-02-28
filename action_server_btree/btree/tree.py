34fe087187c6d6ef5a76bc5d0c0a180b1245650b
tannu945 <tanishaagrawal2015@gmail.com>
2023-02-28 18:32:23 +0530

    chore: update evaluate function.

--- action_server_btree/btree/tree.py
+++ action_server_btree/btree/tree.py
@@ -1,18 +1,19 @@
 from node import Node, NodeState
+from typing import List
 from itertools import islice
-from node_class import non_leaf_node, leaf_node #type: ignore
+from node_class import non_leaf_node, leaf_node
 
 """Global Variables"""
-non_leaf_dict = ["pickup", "hello"]
-leaf_dict = ["tip_availability","tip"]
-retry_count: int = 0
+non_leaf_nodes: List[str] = ["prepare_tip_for_pickup", "pick_up_using_orchestrator", "move_tip_slider_to_pos", "tip_available_in_tray", "caught_tip_firm_and_orient", "load_new_tray", "discard_tip", "discard_tip_success"]
+leaf_nodes: List[str] = ["pick_up", "discard_current_tray", "move_tip_slider", "slider_move_load", "load_next_tray", "goto_discard_pos", "prepare_to_discard", "eject_tip"]
+decision_nodes: List[str] = ["tip_available", "pickup_success", "discard_success", "tray_available", "load_success", "slider_reached", "discard_tip_success", "retry_Count_below_threshold"]
+current_node: int = 0
 
 class Tree:
     _root: Node = None #type: ignore
 
     def Start(self):
         Tree._root = self.SetupTree()
-        current_node = Node.children[0]
     
     def Update(self):
         if Tree._root!=None:
@@ -27,9 +28,10 @@ class Sequence(Node):
 
     def Evaluate(self, _node, _timestamp) -> NodeState:
         anychildisrunning = False
-        for node in islice(Node.children, None, Node.children.index(_node)+1):
+        global current_node
+        for node in islice(Node.children, current_node, Node.children.index(_node)+1):
             """Runs the loop for all the nodes from starting till the given event/node."""
-            if node in leaf_dict:
+            if node in leaf_nodes:
                 """Checks if the given node is leaf node. If yes, then it triggers the evaluate function of leaf node and returns the state of the leaf node."""
                 match leaf_node().Evaluate(_node, _timestamp):
                     case NodeState.FAILURE:
@@ -38,8 +40,19 @@ class Sequence(Node):
                     case NodeState.SUCCESS:
                         state = NodeState.SUCCESS
                         continue
+                    case NodeState.ERROR:
+                        state = NodeState.ERROR
+                        return state
+                    case NodeState.EXCEPTION:
+                        value = Node().getData("retry_count")
+                        if value < 3:
+                            Node().setData("retry_count",  value+1)
+                            state = NodeState.EXCEPTION
+                        else:
+                            state = NodeState.ERROR
+                        return state
                     
-            elif node in non_leaf_dict:
+            elif node in non_leaf_nodes:
                 """Checks if the given node is non-leaf node. If yes, then it triggers the ecaluate function of non-leaf node and returns the state of the non-leaf node."""
                 match non_leaf_node().Evaluate(_node, _timestamp):
                     case NodeState.FAILURE:
@@ -51,7 +64,21 @@ class Sequence(Node):
                     case NodeState.RUNNING:
                         anychildisrunning = True
                         continue
+                    case NodeState.ERROR:
+                        state = NodeState.ERROR
+                        return state
+                    case NodeState.EXCEPTION:
+                        state = NodeState.EXCEPTION
+                        value = Node().getData("retry_count")
+                        if value < 3:
+                            Node().setData("retry_count",  value+1)
+                            state = NodeState.EXCEPTION
+                        else:
+                            state = NodeState.ERROR             
+                        return state
+                    
         state = NodeState.RUNNING if anychildisrunning else NodeState.SUCCESS
+        current_node = Node.children.index(_node)
         return state
     
 
@@ -61,10 +88,10 @@ class Selector(Node):
 
     def Evaluate(self, _node, _timestamp) -> NodeState:
         anychildisrunning = False
-
-        for node in islice(Node.children, None, Node.children.index(_node)+1):
+        global current_node, retry_count
+        for node in islice(Node.children, current_node, Node.children.index(_node)+1):
             """Runs the loop for all the nodes from starting till the given event/node."""
-            if node in non_leaf_dict:
+            if node in non_leaf_nodes:
                 """Checks if the given node is non-leaf node. If yes, then it triggers the evaluate function of non-leaf node and returns the state of the non-leaf node."""
                 match non_leaf_node().Evaluate(node = _node, timestamp=_timestamp):
                     case NodeState.FAILURE:
@@ -76,8 +103,19 @@ class Selector(Node):
                     case NodeState.RUNNING:
                         anychildisrunning = True
                         continue
-                    
-            elif node in leaf_dict:
+                    case NodeState.ERROR:
+                        state = NodeState.ERROR
+                        return state
+                    case NodeState.EXCEPTION:
+                        value = Node().getData("retry_count")
+                        if value < 3:
+                            Node().setData("retry_count",  value+1)
+                            state = NodeState.EXCEPTION
+                        else:
+                            state = NodeState.ERROR
+                        return state
+
+            elif node in leaf_nodes:
                 """Checks if the given node is leaf node. If yes, then it triggers the evaluate function of leaf node and returns the state of the leaf node."""
                 match leaf_node().Evaluate(node = _node, timestamp=_timestamp):
                     case NodeState.FAILURE:
@@ -86,5 +124,17 @@ class Selector(Node):
                     case NodeState.SUCCESS:
                         state = NodeState.SUCCESS
                         return NodeState.SUCCESS
+                    case NodeState.ERROR:
+                        state = NodeState.ERROR
+                        return state
+                    case NodeState.EXCEPTION:
+                        value = Node().getData("retry_count")
+                        if value < 3:
+                            Node().setData("retry_count",  value+1)
+                            state = NodeState.EXCEPTION
+                        else:
+                            state = NodeState.ERROR
+                        return state
         state = NodeState.RUNNING if anychildisrunning else NodeState.FAILURE
+        current_node = Node.children.index(_node)
         return state
