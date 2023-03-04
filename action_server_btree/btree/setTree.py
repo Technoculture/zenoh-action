431bcf9395014c2bcf7e2264e4627a63f6874b35
tannu945 <tanishaagrawal2015@gmail.com>
2023-03-04 18:48:06 +0530

    chore:  tried to update setTree.

--- action_server_btree/btree/setTree.py
+++ action_server_btree/btree/setTree.py
@@ -3,12 +3,11 @@ from tree import Tree, Selector, Sequence
 class SetTree(Tree):
     def SetupTree(self):
         # Create the tree structure
-        root = Selector(["get_tip"])
-        level1 = Sequence(["prepare_tray", "discard_success", Selector(["tip_available"]), Selector(["pickup_success"])])
-        level2_1 = Selector(["tip_available_in_tray"])
-        level3_1 = Sequence(["discard_current_tray", Selector(["discard_success"])])
-        level3_2 = Sequence(["pick_up", Selector(["pickup_success"])])
-        return root
+        level1 = Sequence(["get_tip"])
+        level1.AddChild([Selector(["tip_available"])])
+        level2 = Selector(["pickup_success"])
+        level2.AddChild([Sequence(["move_to_tip", "pick_tip"])])
+        return level1
 
 if __name__ == "__main__":
     tree = SetTree()
