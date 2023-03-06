bdcf5277ee952c4d85db388b3acb7fb630948888
tannu945 <tanishaagrawal2015@gmail.com>
2023-03-06 18:50:04 +0530

    chore: updated setTree

--- action_server_btree/btree/setTree.py
+++ action_server_btree/btree/setTree.py
@@ -4,9 +4,8 @@ class SetTree(Tree):
     def SetupTree(self):
         # Create the tree structure
         level1 = Sequence(["get_tip"])
-        level1.AddChild([Selector(["tip_available"])])
-        level2 = Selector(["pickup_success"])
-        level2.AddChild([Sequence(["move_to_tip", "pick_tip"])])
+        level1.AddChild([Selector(["tip_available", "prepare_tip_for_pickup", "tip_available_in_tray", "discard_current_tray"]), Selector(["pick_up", "move_tip_slider"])])
+        level1_2.AddChild([])
         return level1
 
 if __name__ == "__main__":
