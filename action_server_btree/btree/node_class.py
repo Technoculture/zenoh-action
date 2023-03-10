bb561b66cb2ed5091a4493ad93af18edce6d6259
tannu945 <tanishaagrawal2015@gmail.com>
2023-03-10 18:45:46 +0530

    chore: changes in evaluate function

--- action_server_btree/btree/node_class.py
+++ action_server_btree/btree/node_class.py
@@ -20,7 +20,9 @@ def decide_hardware_module(node):
     Decide the hardware module based on the node.
     """
     hardware_module = ""
-    tiprm = ["tip_available", "pickup_success", "tip_available_in_tray", "move_tip_slider_to_pos", "discard_current_tray", "move_tip_slider", "slider_reached", "tray_available", "slidr_move_to_load", "load_next_tray", "already_in_pos", "prepare_to_discard"]
+    tiprm = ["tip_available", "pickup_success", "tip_available_in_tray", "move_tip_slider_to_pos", 
+             "discard_current_tray", "move_tip_slider", "slider_reached", "tray_available", 
+             "slider_move_to_load", "load_next_tray", "already_in_pos", "prepare_to_discard"]
     tipchecker = ["discard_tip_success", "caught_tip_firm_and_orient"]
     orchestrator = ["pick_up", "caught_tip_firm_and_orient", "goto_discard_pos", "discard_tip_success"]
     pipette = ["load_success", "discard_success", "eject_tip", "discard_tip_success"]
@@ -32,10 +34,11 @@ def decide_hardware_module(node):
         hardware_module = "Orchestrator"
     elif node in pipette:
         hardware_module = "Pipette"
-    print(f"Hardware Module: {hardware_module}")
     return hardware_module
 
 def check_node_valid(node) -> bool:
+    if Node()._datacontext == {}:
+        Node().setData(node, "Running")
     node_list = islice(Node.children, 0, Node.children.index(node))
     for _node in node_list:
         if _node not in Node._datacontext.keys():
@@ -49,7 +52,13 @@ class non_leaf_node(Node):
     def Evaluate(self, node: Any, timestamp: Any) -> NodeState:
         if check_node_valid(node=node):
             if Node().getData(node) == "Running":
-                state = NodeState.RUNNING
+                Node().setData(node, "Success")
+                state = NodeState.SUCCESS
+            else:
+                Node().setData(node, "Running")
+                state = NodeState.SUCCESS
+        else:
+            state = NodeState.FAILURE
         return state
 
 
@@ -59,18 +68,29 @@ class leaf_node(Node):
 
     def Evaluate(self, node: Any, timestamp: Any) -> NodeState:
         hardware_module = decide_hardware_module(node)
-        if hardware_module is not None:
+        if check_node_valid(node=node) == False:
+            Node().setData(node, "Failure")
+            state = NodeState.FAILURE
+            return state
+        if hardware_module != "":
             result = get_status(f"{hardware_module}/trigger?timestamp={timestamp}&event={node}")
-            if result["response_type"] == "Accepted":
-                state = NodeState.SUCCESS
-            elif result["response_type"] == "Error":
-                state = NodeState.ERROR
-            elif result["response_type"] == "Exception":
-                state = NodeState.EXCEPTION
+            if result != {}:
+                if result["response_type"] == "Accepted":
+                    Node().setData(node, "Success")
+                    state = NodeState.SUCCESS
+                elif result["response_type"] == "Error":
+                    Node().setData(node, "Error")
+                    state = NodeState.ERROR
+                elif result["response_type"] == "Exception":
+                    Node().setData(node, "Exception")
+                    state = NodeState.EXCEPTION
+                else:
+                    Node().setData(node, "Failure")
+                    state = NodeState.FAILURE
             else:
+                Node().setData(node, "No response")
                 state = NodeState.FAILURE
         else:
+            Node().setData(node, "Success")
             state = NodeState.SUCCESS
-        print(Node()._datacontext)
-        print(f"leaf State: {state}")
         return state
