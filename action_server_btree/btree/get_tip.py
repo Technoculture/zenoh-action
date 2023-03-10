0c95b151692fe3a03a5814b60fc1188f17891fc6
tannu945 <tanishaagrawal2015@gmail.com>
2023-03-10 18:46:14 +0530

    chore: changes in queryable.

--- action_server_btree/btree/get_tip.py
+++ action_server_btree/btree/get_tip.py
@@ -1,5 +1,5 @@
 from contextlib import contextmanager
-from setTree import SetTree #type: ignore
+from setTree import SetTree
 from typing import Iterator
 from node import NodeState
 import logging
@@ -9,10 +9,14 @@ import zenoh #type: ignore
 logging.getLogger().setLevel(logging.DEBUG)
 
 class Queryable:
+    """Queryable class to handle the queries."""
+
     def __init__(self, tree) -> None:
+        """Initialise variables."""
         self.tree = tree
 
     def trigger_queryable_handler(self, query: zenoh.Query) -> None:
+        """ Handle the query for the trigger queryable."""
         logging.debug("Received query: {}".format(query.selector))
         event = query.selector.decode_parameters()
         if event == {}:
@@ -22,26 +26,35 @@ class Queryable:
         else:
             root = self.tree.SetupTree()
             value = root.Evaluate(event.get("event"), event.get("timestamp"))
-            logging.debug(str(value))
-            if value == NodeState.SUCCESS or value == NodeState.RUNNING:
+            if value == NodeState.SUCCESS:
                 payload = {"response_type":"Accepted", "response":"Get Tip Success."}
             else:
                 payload = {"response_type":"Rejected", "response":"Get Tip Failure."}
         query.reply(zenoh.Sample("GetTip/trigger", payload))
 
 class Session:
+    """Session class to handle the session."""
+
     def __init__(self, handler: Queryable) -> None:
+        """Initialise variables."""
         self.handler = handler
-    def open(self):
+
+    def open(self) -> None:
+        """Open the session."""
         self.config = zenoh.Config()
         self.session = zenoh.open(self.config)
         self.trigger_queryable = self.session.declare_queryable("GetTip/trigger", self.handler.trigger_queryable_handler)
-    def close(self):
+
+    def close(self) -> None:
+        """Close the session."""
         self.session.close()
         self.trigger_queryable.undeclare()    
 
 @contextmanager
 def session_manager(handler: Queryable) -> Iterator[Session]:
+    """Context manager for the session.
+        Returns: Iterator[Session]
+    """
     try:
         session = Session(handler)
         session.open()
@@ -52,7 +65,6 @@ def session_manager(handler: Queryable) -> Iterator[Session]:
         session.close()
 
 if __name__ == "__main__":
-    #get_tip = GetTipNode()
     tree = SetTree()
     handler = Queryable(tree)
     with session_manager(handler) as session:
