41a8e3725ec08ce018140160e6a26154337b916e
tannu945 <tanishaagrawal2015@gmail.com>
2023-03-14 18:54:56 +0530

    feat: updated classes

--- action_server_btree/btree/node_class.py
+++ action_server_btree/btree/node_class.py
@@ -20,6 +20,7 @@ def decide_hardware_module(node):
     Decide the hardware module based on the node.
     """
     hardware_module = ""
+
     tiprm = ["tip_available", "pickup_success", "tip_available_in_tray", "move_tip_slider_to_pos", 
              "discard_current_tray", "move_tip_slider", "slider_reached", "tray_available", 
              "slider_move_to_load", "load_next_tray", "already_in_pos", "prepare_to_discard"]
@@ -38,60 +39,62 @@ def decide_hardware_module(node):
     return hardware_module
 
 def check_node_valid(node) -> bool:
+    """ Checks if the node is valid or not. """
+
     if Node()._datacontext == {}:
         Node().setData(node, "Running")
     node_list = islice(Node.children, 0, Node.children.index(node))
     for _node in node_list:
-        if _node not in Node._datacontext.keys():
+        if _node not in Node()._datacontext.keys():
             return False
     return True
 
-class non_leaf_node(Node):
+class non_hardware_node(Node):
     def __init__(self) -> None:
         super().__init__()
 
     def Evaluate(self, node: Any, timestamp: Any) -> NodeState:
         if check_node_valid(node=node):
-            if Node().getData(node) == "Running":
-                Node().setData(node, "Success")
+            if self.getData(node) == "Running":
+                self.setData(node, "Success")
                 state = NodeState.SUCCESS
             else:
-                Node().setData(node, "Running")
+                self.setData(node, "Running")
                 state = NodeState.SUCCESS
         else:
             state = NodeState.FAILURE
         return state
 
 
-class leaf_node(Node):
+class hardware_node(Node):
     def __init__(self) -> None:
         super().__init__()
 
     def Evaluate(self, node: Any, timestamp: Any) -> NodeState:
         hardware_module = decide_hardware_module(node)
         if check_node_valid(node=node) == False:
-            Node().setData(node, "Failure")
+            self.setData(node, "Failure")
             state = NodeState.FAILURE
             return state
         if hardware_module != "":
             result = get_status(f"{hardware_module}/trigger?timestamp={timestamp}&event={node}")
             if result != {}:
                 if result["response_type"] == "Accepted":
-                    Node().setData(node, "Success")
+                    self.setData(node, "Success")
                     state = NodeState.SUCCESS
                 elif result["response_type"] == "Error":
-                    Node().setData(node, "Error")
+                    self.setData(node, "Error")
                     state = NodeState.ERROR
                 elif result["response_type"] == "Exception":
-                    Node().setData(node, "Exception")
+                    self.setData(node, "Exception")
                     state = NodeState.EXCEPTION
                 else:
-                    Node().setData(node, "Failure")
+                    self.setData(node, "Failure")
                     state = NodeState.FAILURE
             else:
-                Node().setData(node, "No response")
+                self.setData(node, "No response")
                 state = NodeState.FAILURE
         else:
-            Node().setData(node, "Success")
+            self.setData(node, "Success")
             state = NodeState.SUCCESS
         return state
